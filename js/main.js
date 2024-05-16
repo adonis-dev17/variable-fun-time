3b0bc127634215a2ba8151cb92907428a784f396
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-16 04:53:18 -0500

    First commit.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//--- Easy
+//create a variable and assign it a number
+let num = 12;
+//minus 10 from that number
+num = num - 10;
+//print that number to the console
+console.log(num);
+//--- Medium
+//create a variable that holds a value from the input
+let captureInput = Number(document.querySelector('#danceDanceRevolution').value);
+//add 25 to that number
+captureInput = captureInput + 25;
+//alert that number
+alert(captureInput);
+//--- Hard
+// let 
+//create a variable that holds the h1
+
+//add an event listener to that element that console logs the sum of the two previous variables
